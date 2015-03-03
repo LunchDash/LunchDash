feaf5cffe6ab8182422db1692eb4079705b72540
Alexander Tu <alextu87@gmail.com>
2015-03-02 23:26:05 -0800

    Initial commit, contains everything from YelpAPI repo. Added a splash screen.

---
+++ app/src/main/java/com/lunchdash/lunchdash/APIs/Keys.java
@@ -0,0 +1,9 @@
+package com.lunchdash.lunchdash.APIs;
+
+public class Keys {
+    public static String yelpConsumerKey = "hjNmE3MjCSXy1zgrb-J4lA";
+    public static String yelpConsumerSecret = "by0nVcUhDcD59CBV03o8IO1PsP4";
+    public static String yelpToken = "t9pMivGuyOTiAMXbd1V-8aIsK04pSptB";
+    public static String yelpTokenSecret = "qFjo9pB_hgD7Et33X64NlRzd5DI";
+}
+
