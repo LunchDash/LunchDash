992f4d57b265352e6b5e5b507bce751cac476b85
Alexander Tu <alextu87@gmail.com>
2015-04-01 05:22:58 -0700

    Added the navbar to the Profile and Settings fragments.

--- app/src/main/java/com/lunchdash/lunchdash/fragments/ProfileFragment.java
+++ app/src/main/java/com/lunchdash/lunchdash/fragments/ProfileFragment.java
@@ -5,11 +5,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.lunchdash.lunchdash.LunchDashApplication;
 import com.lunchdash.lunchdash.R;
+import com.lunchdash.lunchdash.activities.MainActivity;
 import com.lunchdash.lunchdash.models.User;
 import com.squareup.picasso.Picasso;
 
@@ -26,6 +28,15 @@ public class ProfileFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_profile, parent, false);
         User user = LunchDashApplication.user;
         setupViews(v, user);
+
+        ImageButton ibDrawer = (ImageButton) v.findViewById(R.id.ibDrawer); //This button opens/closes the nav drawer
+        ibDrawer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((MainActivity) getActivity()).toggleDrawer();
+            }
+        });
+
         return v;
     }
 
--- app/src/main/java/com/lunchdash/lunchdash/fragments/SettingsFragment.java
+++ app/src/main/java/com/lunchdash/lunchdash/fragments/SettingsFragment.java
@@ -1,25 +1,56 @@
 package com.lunchdash.lunchdash.fragments;
 
-import android.graphics.Color;
+
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.lunchdash.lunchdash.R;
+import com.lunchdash.lunchdash.activities.MainActivity;
 import com.lunchdash.lunchdash.helpers.PreferenceFragment;
 
-public class SettingsFragment extends PreferenceFragment {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.preferences);
-    }
+
+public class SettingsFragment extends Fragment {
 
     @Override
-    public View onCreateView(LayoutInflater layoutInflater, ViewGroup viewGroup, Bundle savedInstanceState) {
-        View view = super.onCreateView(layoutInflater, viewGroup, savedInstanceState);
-        view.setBackgroundColor(Color.WHITE);
-        return view;
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        FragmentManager fm = getChildFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.add(R.id.flSettings, new PrefFragment(), "Preference Fragment");
+        ft.commit();
+
+        ImageButton ibDrawer = (ImageButton) v.findViewById(R.id.ibDrawer); //This button opens/closes the nav drawer
+        ibDrawer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((MainActivity) getActivity()).toggleDrawer();
+            }
+        });
+
+        return v;
     }
+
+    public static class PrefFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+        }
+
+        /*@Override
+        public View onCreateView(LayoutInflater layoutInflater, ViewGroup viewGroup, Bundle savedInstanceState) {
+            View view = super.onCreateView(layoutInflater, viewGroup, savedInstanceState);
+            view.setBackgroundColor(Color.WHITE);
+            return view;
+        }*/
+    }
+
 }
